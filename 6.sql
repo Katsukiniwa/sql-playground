434da54074f27d10d3bcf74e21198824059a1bcc
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-08-08 15:31:23 +0900

    complete section6

--- 6.sql
+++ 6.sql
@@ -1,3 +1,12 @@
 select id as ID, name as 名前, height as 身長 from members where gender = 'F';
 
 select m.id, m.height from members m where m.gender = 'F' order by m.id;
+
+select name as n, gender as g
+from members
+where g = 'F'
+order by n
+
+select m.name as n, m.gender as g from members as m where m.gender = "F" order by m.id;
+
+select name from members where name like '%ン';
